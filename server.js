08f31b23d86203c0a405900e8893514466983a41
Sandrome <sudayavarman@salesforce.com>
2019-10-20 23:32:49 +0530

    New

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/', function (req, res) {
   
   app.post('/', function (req, res) {
     let city = req.body.city;
-    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    let url = `https://r6tab.com/api/search.php?platform=uplay&search=${city}`
 
     request(url, function (err, response, body) {
       if(err){
@@ -25,8 +25,8 @@ app.get('/', function (req, res) {
           res.render('index', {weather: null, error: 'Error, please try again'});
         } else {
           let weatherText = `It's ${(5/9) * (weather.main.temp - 32)} degrees in ${weather.name}!`;
-          res.render('index', {weather: weatherText, error: null});
-          //res.json((5/9) * (weather.main.temp - 32));
+          //res.render('index', {weather: weatherText, error: null});
+          res.json(weather);
         }
       }
     });
