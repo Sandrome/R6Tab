5ffc3ee26610f234d35a6ec47a124b036cd7f46f
Sandrome <sudayavarman@salesforce.com>
2019-10-21 12:34:41 +0530

    Commit new

--- server.js
+++ server.js
@@ -2,31 +2,30 @@ const express = require('express');
 const app = express()
 const bodyParser = require('body-parser');
 const request = require('request');
-const apiKey = '5931216f620d49d344b751d74ea3dc45';
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
 app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    res.render('index', {weather: null, error: null});
+    res.render('index', {player: null, error: null});
   })
   
   app.post('/', function (req, res) {
-    let city = req.body.city;
+    let city = req.body.;
     let url = `https://r6tab.com/api/search.php?platform=uplay&search=${city}`
 
     request(url, function (err, response, body) {
       if(err){
-        res.render('index', {weather: null, error: 'Error, please try again'});
+        res.render('index', {player: null, error: 'Error, please try again'});
       } else {
-        let weather = JSON.parse(body)
-        if(weather.results == undefined){
-          res.render('index', {weather: null, error: 'Error, please try again'});
+        let player = JSON.parse(body)
+        if(player.results == undefined){
+          res.render('index', {player: null, error: 'Error, please try again'});
         } else {
-          let weatherText = `It's ${weather.results[0].p_name}`;
-          res.render('index', {weather: weatherText, error: null});
-          //res.json(weather);
+          let playerText = `It's ${player.results[0].p_name}`;
+          res.render('index', {player: playerText, error: null});
+          //res.json(player);
         }
       }
     });
